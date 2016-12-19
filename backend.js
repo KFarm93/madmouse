413e56faf6e24ad0ddbd9724edbf00e8a722a1a1
Kevin Farmer <farmekp0@sewanee.edu>
2016-12-19 17:04:41 -0500

    Added mouse pic rotation and finished maze 1

--- backend.js
+++ backend.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var app = require('express')();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var x = 18;
 var y = 90;
+var src = "images/mouse_down.png";
 
 app.use(express.static('public'));
 
@@ -15,24 +16,28 @@ io.on('connection', function(socket){
     if (data[0] === 'top') {
       if (data[1] === '-=50' && y >= 140) {
         y -= 50;
-        console.log(x, y);
+        src = 'images/mouse_up.png';
+        console.log("up");
       }
       else if (data[1] === '+=50' && y <= 690) {
         y += 50;
-        console.log(x, y);
+        src = 'images/mouse_down.png';
+        console.log("down");
       }
     }
     if (data[0] === 'left') {
       if (data[1] === '-=55' && x >= 68.5) {
         x -= 50.5;
-        console.log(x, y);
+        src = 'images/mouse_left.png';
+        console.log("left");
       }
       else if (data[1] === '+=55' && x <= 624) {
         x += 50.5;
-        console.log(x, y);
+        src = 'images/mouse_right.png';
+        console.log("right");
       }
     }
-    io.emit('replicate', [x, y]);
+    io.emit('replicate', [x, y, src]);
   });
 });
 
